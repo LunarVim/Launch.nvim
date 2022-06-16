a55721790cc88898f49bac87bddb504836e02e5a
Christian Chiarulli <chris.machine@pm.me>
2022-06-15 23:52:02 -0400

    remove notification

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,6 +2,5 @@ local colorscheme = "darkplus"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
