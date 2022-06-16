8b9ec3bffe8c8577042baf07c75408532a733fea
ChristianChiarulli <chris.machine@pm.me>
2022-06-16 11:25:04 -0400

    format

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -11,7 +11,7 @@ telescope.setup {
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
-		file_ignore_patterns = { ".git/", "node_modules" },
+    file_ignore_patterns = { ".git/", "node_modules" },
 
     mappings = {
       i = {
