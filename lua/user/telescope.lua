caa744d87911dbd2e25ef3014b99611447b7c6ed
ChristianChiarulli <chris.machine@pm.me>
2022-06-16 11:24:48 -0400

    update file ignore patterns

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -11,6 +11,7 @@ telescope.setup {
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
+		file_ignore_patterns = { ".git/", "node_modules" },
 
     mappings = {
       i = {
