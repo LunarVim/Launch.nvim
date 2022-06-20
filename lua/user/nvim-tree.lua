3640d5b3559dc6998d773b8ab37d73beef1e7fee
Christian Chiarulli <chris.machine@pm.me>
2022-06-20 01:13:41 -0400

    update focused file for nvimtree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -11,6 +11,10 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
   renderer = {
     root_folder_modifier = ":t",
     icons = {
