7e56fd86ca8b6ce6dc5dd537fa34b044080103d6
rjshrjndrn <rjshrjndrn@gmail.com>
2022-06-21 09:46:05 +0200

    chore(plugins): tmux plugins
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,6 +59,9 @@ return packer.startup(function(use)
   use { "lewis6991/impatient.nvim", commit = "969f2c5c90457612c09cf2a13fee1adaa986d350" }
   use { "lukas-reineke/indent-blankline.nvim", commit = "6177a59552e35dfb69e1493fd68194e673dc3ee2" }
   -- use { "goolord/alpha-nvim", commit = "ef27a59e5b4d7b1c2fe1950da3fe5b1c5f3b4c94" }
+  -- Tmux
+  use {'christoomey/vim-tmux-navigator'}
+  use {'tmux-plugins/vim-tmux-focus-events'}
 
   -- Colorschemes
   use { "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" }
