49d8b68a139421b9f7cbe80650b30d6bad84c540
Pascal Hubrecht <hansemschnokeloch@users.noreply.github.com>
2022-06-26 05:48:16 +0200

    Remove indent_blankline_ part (#9)
    indent_blankline_ part has to be removed from configuration variables

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -4,14 +4,13 @@ if not status_ok then
 end
 
 indent_blankline.setup {
+  char = "▏",
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = true,
+  use_treesitter = true,
   show_current_context = true,
-  indent_blankline_char = "▏",
-  indent_blankline_show_trailing_blankline_indent = false,
-  indent_blankline_show_first_indent_level = true,
-  indent_blankline_use_treesitter = true,
-  indent_blankline_show_current_context = true,
-  indent_blankline_buftype_exclude = { "terminal", "nofile" },
-  indent_blankline_filetype_exclude = {
+  buftype_exclude = { "terminal", "nofile" },
+  filetype_exclude = {
     "help",
     "packer",
     "NvimTree",
