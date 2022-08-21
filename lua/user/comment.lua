b03581727dd8e600bf12362fc56cf315d7fd3660
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-08-21 16:40:59 +0530

    check for jsx/tsx filetypes before loading hooks

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -5,18 +5,20 @@ end
 
 comment.setup {
   pre_hook = function(ctx)
-    local U = require "Comment.utils"
+    if vim.bo.filetype == 'typescriptreact' then
+      local U = require "Comment.utils"
 
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require("ts_context_commentstring.utils").get_visual_start_location()
+      local location = nil
+      if ctx.ctype == U.ctype.block then
+        location = require("ts_context_commentstring.utils").get_cursor_location()
+      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+        location = require("ts_context_commentstring.utils").get_visual_start_location()
+      end
+
+      return require("ts_context_commentstring.internal").calculate_commentstring {
+        key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+        location = location,
+      }
     end
-
-    return require("ts_context_commentstring.internal").calculate_commentstring {
-      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-      location = location,
-    }
   end,
 }
