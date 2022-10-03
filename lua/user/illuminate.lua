01adeb9395bd43955ce32a1e40fa68c4c93afc6d
Rishabh <jharishabh2k3@gmail.com>
2022-10-03 14:33:39 +0530

    fix: call illuminate with pcall

--- lua/user/illuminate.lua
+++ lua/user/illuminate.lua
@@ -1,8 +1,13 @@
+local status_ok, illuminate = pcall(require, "illuminate")
+if not status_ok then
+	return
+end
+
 vim.g.Illuminate_ftblacklist = {'alpha', 'NvimTree'}
 vim.api.nvim_set_keymap('n', '<a-n>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<a-p>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
 
-require("illuminate").configure {
+illuminate.configure {
   providers = {
     "lsp",
     "treesitter",
