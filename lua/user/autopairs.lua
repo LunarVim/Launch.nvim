b85b74aa6e201178d461f3b42e60950c2920adb8
Rishabh <jharishabh2k3@gmail.com>
2022-10-07 13:41:39 +0530

    fix: slimmed down the gitsign config and formatted the autopairs config

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -1,19 +1,19 @@
 -- Setup nvim-cmp.
 local status_ok, npairs = pcall(require, "nvim-autopairs")
 if not status_ok then
-  return
+	return
 end
 
-npairs.setup {
-  check_ts = true, -- treesitter integration
-  disable_filetype = { "TelescopePrompt" },
-  ts_config = {
+npairs.setup({
+	check_ts = true, -- treesitter integration
+	disable_filetype = { "TelescopePrompt" },
+	ts_config = {
 		lua = { "string", "source" },
 		javascript = { "string", "template_string" },
 		java = false,
 	},
-  
-  fast_wrap = {
+
+	fast_wrap = {
 		map = "<M-e>",
 		chars = { "{", "[", "(", '"', "'" },
 		pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
@@ -24,11 +24,11 @@ npairs.setup {
 		highlight = "PmenuSel",
 		highlight_grey = "LineNr",
 	},
-}
+})
 
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-  return
+	return
 end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done {})
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({}))
--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -12,28 +12,19 @@ gitsigns.setup {
     changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-  linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
     interval = 1000,
     follow_files = true,
   },
   attach_to_untracked = true,
-  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
-    ignore_whitespace = false,
-  },
-  current_line_blame_formatter_opts = {
-    relative_time = false,
   },
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
-  max_file_length = 40000,
   preview_config = {
     -- Options passed to nvim_open_win
     border = "single",
@@ -41,8 +32,5 @@ gitsigns.setup {
     relative = "cursor",
     row = 0,
     col = 1,
-  },
-  yadm = {
-    enable = false,
   },
 }
