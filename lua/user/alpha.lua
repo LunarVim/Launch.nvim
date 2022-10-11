01bb9eb78e461fa2c45f55838ed680ece09efb65
Alexis <74944536+Alexis12119@users.noreply.github.com>
2022-10-12 06:01:35 +0800

    Fix possible conflict on windows
    change `~/.config/nvim/init.lua` to `$MYVIMRC`

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -18,7 +18,7 @@ dashboard.section.buttons.val = {
   dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
   dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
