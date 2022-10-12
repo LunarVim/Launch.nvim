3231786de1a2030046e9c9afa1e715efa449356b
Rishabh <jharishabh2k3@gmail.com>
2022-10-12 11:43:27 +0530

    feat:updated the dap-ui and fixed the lualine

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -19,7 +19,8 @@ dap_install.config("python", {})
 -- add other configs here
 
 dapui.setup({
-	icons = { expanded = "▾", collapsed = "▸" },
+	expand_lines = true,
+	icons = { expanded = "", collapsed = "", circular = "" },
 	mappings = {
 		-- Use a table to apply multiple mappings
 		expand = { "<CR>", "<2-LeftMouse>" },
@@ -29,48 +30,33 @@ dapui.setup({
 		repl = "r",
 		toggle = "t",
 	},
-	-- Expand lines larger than the window
-	-- Requires >= 0.7
-	expand_lines = vim.fn.has("nvim-0.7"),
-	-- Layouts define sections of the screen to place windows.
-	-- The position can be "left", "right", "top" or "bottom".
-	-- The size specifies the height/width depending on position. It can be an Int
-	-- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-	-- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-	-- Elements are the elements shown in the layout (in order).
-	-- Layouts are opened in order so that earlier layouts take priority in window sizing.
 	layouts = {
 		{
 			elements = {
-				-- Elements can be strings or table with id and size keys.
-				{ id = "scopes", size = 0.25 },
-				"breakpoints",
-				-- "stacks",
-				-- "watches",
+				{ id = "scopes", size = 0.33 },
+				{ id = "breakpoints", size = 0.17 },
+				{ id = "stacks", size = 0.25 },
+				{ id = "watches", size = 0.25 },
 			},
-			size = 40, -- 40 columns
+			size = 0.33,
 			position = "right",
 		},
 		{
 			elements = {
-				"repl",
-				"console",
+				{ id = "repl", size = 0.45 },
+				{ id = "console", size = 0.55 },
 			},
-			size = 0.25, -- 25% of total lines
+			size = 0.27,
 			position = "bottom",
 		},
 	},
 	floating = {
-		max_height = nil, -- These can be integers or a float between 0 and 1.
-		max_width = nil, -- Floats will be treated as percentage of your screen.
-		border = "single", -- Border style. Can be "single", "double" or "rounded"
+		max_height = 0.9,
+		max_width = 0.5, -- Floats will be treated as percentage of your screen.
+		border = vim.g.border_chars, -- Border style. Can be 'single', 'double' or 'rounded'
 		mappings = {
 			close = { "q", "<Esc>" },
 		},
-	},
-	windows = { indent = 1 },
-	render = {
-		max_type_length = nil, -- Can be integer or nil.
 	},
 })
 
