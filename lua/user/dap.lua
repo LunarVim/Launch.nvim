0e65f504f634026f5765ce6a092612d385d6306d
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2022-10-13 11:04:57 +0530

    feat(dap): updated the dap and dapui to neovim 0.8 (#67)
    * feat(dap): updated the dap and dapui to neovim 0.8
    
    * feat:updated the dap-ui and fixed the lualine

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -1,50 +1,75 @@
 local dap_status_ok, dap = pcall(require, "dap")
 if not dap_status_ok then
-  return
+	return
 end
 
 local dap_ui_status_ok, dapui = pcall(require, "dapui")
 if not dap_ui_status_ok then
-  return
+	return
 end
 
 local dap_install_status_ok, dap_install = pcall(require, "dap-install")
 if not dap_install_status_ok then
-  return
+	return
 end
 
-dap_install.setup {}
+dap_install.setup({})
 
 dap_install.config("python", {})
 -- add other configs here
 
-dapui.setup {
-  sidebar = {
-    elements = {
-      {
-        id = "scopes",
-        size = 0.25, -- Can be float or integer > 1
-      },
-      { id = "breakpoints", size = 0.25 },
-    },
-    size = 40,
-    position = "right", -- Can be "left", "right", "top", "bottom"
-  },
-  tray = {
-    elements = {},
-  },
-}
+dapui.setup({
+	expand_lines = true,
+	icons = { expanded = "", collapsed = "", circular = "" },
+	mappings = {
+		-- Use a table to apply multiple mappings
+		expand = { "<CR>", "<2-LeftMouse>" },
+		open = "o",
+		remove = "d",
+		edit = "e",
+		repl = "r",
+		toggle = "t",
+	},
+	layouts = {
+		{
+			elements = {
+				{ id = "scopes", size = 0.33 },
+				{ id = "breakpoints", size = 0.17 },
+				{ id = "stacks", size = 0.25 },
+				{ id = "watches", size = 0.25 },
+			},
+			size = 0.33,
+			position = "right",
+		},
+		{
+			elements = {
+				{ id = "repl", size = 0.45 },
+				{ id = "console", size = 0.55 },
+			},
+			size = 0.27,
+			position = "bottom",
+		},
+	},
+	floating = {
+		max_height = 0.9,
+		max_width = 0.5, -- Floats will be treated as percentage of your screen.
+		border = vim.g.border_chars, -- Border style. Can be 'single', 'double' or 'rounded'
+		mappings = {
+			close = { "q", "<Esc>" },
+		},
+	},
+})
 
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+	dapui.open()
 end
 
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
 
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -19,7 +19,7 @@ local diagnostics = {
 local diff = {
   "diff",
   colored = false,
-  symbols = { added = "", modified = "", removed = "" }, -- changes diff symbols
+  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
   cond = hide_in_width,
 }
 
