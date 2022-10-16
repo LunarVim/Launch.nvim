fea7d38cf8f28ff106bff18b48e5953c1ae8f399
Nelson Guerrero <nelsong@DESKTOP-PETPEIQ.localdomain>
2022-10-16 07:55:49 -0400

    fix: removed whitespace in line.

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -13,7 +13,7 @@ configs.setup({
   -- ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  
+
   highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
