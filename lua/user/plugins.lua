1497a530a3d8b12c8010bcc0886da5e6d2b08910
HunderaAwoke <hunderaweke@gmail.com>
2022-11-07 05:54:01 +0300

    added git blame

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -164,7 +164,7 @@ return packer.startup(function(use)
   use({ "lukas-reineke/lsp-format.nvim", commit = "b611bd6cea82ccc127cf8fd781a1cb784b0d6d3c" })
   -- Git
   use({ "lewis6991/gitsigns.nvim", commit = "f98c85e7c3d65a51f45863a34feb4849c82f240f" })
-
+  use("f-person/git-blame.nvim")
   -- DAP
   use({ "mfussenegger/nvim-dap", commit = "6b12294a57001d994022df8acbe2ef7327d30587" })
   use({ "rcarriga/nvim-dap-ui", commit = "1cd4764221c91686dcf4d6b62d7a7b2d112e0b13" })
