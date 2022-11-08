5084c986960bec1c83e39c8391eb761822959db1
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2022-11-08 22:07:37 +0800

    Fix(Floating window): words breaking halfway

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -39,3 +39,4 @@ vim.opt.shortmess:append "c"                    -- hide all the completion messa
 vim.opt.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the previous/next line when the beginning/end of line is reached
 vim.opt.iskeyword:append("-")                   -- treats words with `-` as single words
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- This is a sequence of letters which describes how automatic formatting is to be done
+vim.opt.linebreak = true
