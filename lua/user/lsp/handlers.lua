26b7912c580cb5e98d0be7eed63709f2e2c80b71
Hencter <hencter@foxmail.com>
2022-12-02 17:37:38 +0000

    fix: change to the default_capabilities
    reason: update_capabilities deprecated

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -7,7 +7,7 @@ end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
 	local signs = {
