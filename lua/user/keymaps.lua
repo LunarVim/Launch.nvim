9d626ef9af01766fce371873f06e564617196749
Rishabh <jharishabh2k3@gmail.com>
2022-12-08 23:29:15 +0530

    fix(keymap): moved keymap from handlers.lua to keymaps.lua as it wasnt working with null-ls there

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -78,3 +78,6 @@ keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
 keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
 keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
 keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
+
+-- Lsp
+keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
