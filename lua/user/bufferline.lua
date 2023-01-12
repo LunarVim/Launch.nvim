88e73c82d56649149776584ad15d9022ea286c7f
bruteforks <116930331+bruteforks@users.noreply.github.com>
2023-01-12 02:36:27 +0000

    Bdelete command isn't available when certain plugins aren't installed yet throwing error on startup

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -5,8 +5,8 @@ end
 
 bufferline.setup {
   options = {
-    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+    close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
   },
