82e977a65da8767c05ae7152d886e52c2ef70519
rjshrjndrn <rjshrjndrn@gmail.com>
2023-01-21 10:55:49 +0100

    chore(plugins): which key plugin
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,6 +59,12 @@ return packer.startup(function(use)
   use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
   use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
   use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
+  use { "folke/which-key.nvim", commit = "e4fa445065a2bb0bbc3cca85346b67817f28e83e",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {}
+  end}
 
   -- Colorschemes
   use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
