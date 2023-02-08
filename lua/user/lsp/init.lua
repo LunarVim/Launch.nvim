fac00725778aa3fc896ec690831f617297b6f0fd
Rishabh <jharishabh2k3@gmail.com>
2023-02-08 13:12:33 +0530

    progress to lazy

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,8 +1,2 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "user.lsp.mason"
-require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require("rj.plugins.lsp.attach")
+require("rj.plugins.lsp.diagnostic")
