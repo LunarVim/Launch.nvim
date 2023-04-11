194cc2afd4cea5ff59718d7b5d0368bf8b8cf915
MickTheRus <nikolai.romanchenko@outlook.com>
2023-04-11 08:19:06 -0600

    Lazy's opts is funky.

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -14,28 +14,28 @@ local M = {
   },
 }
 
-M.opts = {
-  check_ts = true, -- treesitter integration
-  disable_filetype = { "TelescopePrompt" },
-  ts_config = {
-    lua = { "string", "source" },
-    javascript = { "string", "template_string" },
-    java = false,
-  },
-  fast_wrap = {
-    map = "<M-e>",
-    chars = { "{", "[", "(", '"', "'" },
-    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-    offset = 0, -- Offset from pattern match
-    end_key = "$",
-    keys = "qwertyuiopzxcvbnmasdfghjkl",
-    check_comma = true,
-    highlight = "PmenuSel",
-    highlight_grey = "LineNr",
-  },
-}
-
 function M.config()
+  require("nvim-autopairs").setup {
+    check_ts = true, -- treesitter integration
+    disable_filetype = { "TelescopePrompt" },
+    ts_config = {
+      lua = { "string", "source" },
+      javascript = { "string", "template_string" },
+      java = false,
+    },
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+      offset = 0, -- Offset from pattern match
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+      highlight = "PmenuSel",
+      highlight_grey = "LineNr",
+    },
+  }
+
   local cmp_autopairs = require "nvim-autopairs.completion.cmp"
   local cmp = require "cmp"
 
