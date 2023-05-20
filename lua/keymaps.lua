ef5c6fa0fd879a0be347ee8c05d502093808b46f
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2023-05-20 15:45:35 +0200

    simplify keybind
    `"_dP` does the same as just `P` in visual mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -39,7 +39,7 @@ keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
 keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
 
 -- Better paste
-keymap("v", "p", '"_dP', opts)
+keymap("v", "p", 'P', opts)
 
 -- Insert --
 -- Press jk fast to enter
