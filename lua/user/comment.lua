4ce51ad2620eaf3e6b19603199b935423ff56697
psvelayudhan <72850275+psvelayudhan@users.noreply.github.com>
2023-05-21 09:41:25 -0400

    Update nvim-ts-context-commentstring dependency commit in comment.lua (#130)

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -6,7 +6,7 @@ local M = {
     {
       "JoosepAlviste/nvim-ts-context-commentstring",
       event = "VeryLazy",
-      commit = "a0f89563ba36b3bacd62cf967b46beb4c2c29e52",
+      commit = "729d83ecb990dc2b30272833c213cc6d49ed5214",
     },
   },
 }
