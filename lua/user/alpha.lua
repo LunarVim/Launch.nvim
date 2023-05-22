b063301c1bc545b81ff5194ca5d111bf9a2cb74f
Rishabh <jharishabh2k3@gmail.com>
2023-05-22 20:58:48 +0530

    i think i fixed all of them

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -19,7 +19,7 @@ function M.config()
     dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
     dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
     dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-    dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+    dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
     dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
     dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
     dashboard.button("q", " " .. " Quit", ":qa<CR>"),
