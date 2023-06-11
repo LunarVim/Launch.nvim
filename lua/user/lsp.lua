170452c29ccd6c3f07281a4085f033740b9d083b
Tomas Gareau <tggareau@gmail.com>
2023-06-11 12:51:29 -0600

    fix: defer cmp_nvim_lsp require to config
    If cmp_nvim_lsp has not already been installed, this require will fail.
    Moving it to the config allows lazy to find it listed in the
    dependencies above and install it _before_ we try requiring it.

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -10,8 +10,9 @@ local M = {
   },
 }
 
-local cmp_nvim_lsp = require "cmp_nvim_lsp"
 function M.config()
+  local cmp_nvim_lsp = require "cmp_nvim_lsp"
+
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
