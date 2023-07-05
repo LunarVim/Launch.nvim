c27b4790216f2cddca771e5a9ac91783dad21586
Alexander Sandukovskiy <asandukovskiy@gmail.com>
2023-07-05 17:53:19 +0200

    Remove  since it is default in lazy

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -57,7 +57,6 @@ end
 M = {
   "ravenxrz/DAPInstall.nvim",
   commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de",
-  lazy = true,
   config = function()
     require("dap_install").setup {}
     require("dap_install").config("python", {})
--- lua/user/mason.lua
+++ lua/user/mason.lua
@@ -7,7 +7,6 @@ local M = {
     {
       "williamboman/mason-lspconfig.nvim",
       commit = "93e58e100f37ef4fb0f897deeed20599dae9d128",
-      lazy = true,
     },
   },
 }
