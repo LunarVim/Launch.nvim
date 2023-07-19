52bdae9ce30737e0d6433c805e68934ce3a6e665
Christian Chiarulli <chris.machine@pm.me>
2023-07-19 12:51:15 -0400

    ask before deleting if edited

--- lua/user/bufdelete.lua
+++ lua/user/bufdelete.lua
@@ -6,7 +6,9 @@ local M = {
 function M.config()
   local keymap = vim.keymap.set
   local opts = { noremap = true, silent = true }
-  keymap("n", "Q", "<cmd>Bdelete!<CR>", opts)
+  -- keymap("n", "Q", "<cmd>Bdelete!<CR>", opts)
+  keymap("n", "Q", ":lua require('bufdelete').bufdelete(0, false)<cr>", opts)
+
 end
 
 return M
