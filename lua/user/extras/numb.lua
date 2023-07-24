fc272353bedcdd90f2dcb0a98f63a878b1667089
Christian Chiarulli <chris.machine@pm.me>
2023-07-23 21:40:20 -0400

    update

--- lua/user/extras/numb.lua
+++ lua/user/extras/numb.lua
@@ -4,11 +4,7 @@ local M = {
 }
 
 function M.config()
-  local status_ok, numb = pcall(require, "numb")
-  if not status_ok then
-    return
-  end
-  numb.setup {
+  require("numb").setup {
     show_numbers = true, -- Enable 'number' for the window while peeking
     show_cursorline = true, -- Enable 'cursorline' for the window while peeking
   }
