329b1cd09f5a9eced24765fb93fec9e3bb1776c9
Tiago Muniz de Araujo <tiagomuniz130@gmail.com>
2023-10-03 14:53:48 -0300

    chore(which-key): config which-key to look prettier (#150)

--- lua/options.lua
+++ lua/options.lua
@@ -41,3 +41,6 @@ vim.opt.whichwrap:append "<,>,[,],h,l"          -- keys allowed to move to the p
 vim.opt.iskeyword:append "-"                    -- treats words with `-` as single words
 vim.opt.formatoptions:remove { "c", "r", "o" }  -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
+
+-- Globals
+vim.g.mapleader = " "
--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -2,10 +2,16 @@ local M = {
   "folke/which-key.nvim",
   commit = "5224c261825263f46f6771f1b644cae33cd06995",
   event = "VeryLazy",
+  opts = {
+    icons = {
+      group = "", -- symbol prepended to a group
+    },
+  }
 }
 
-function M.config()
-  require("which-key").setup {}
+function M.config(_, opts)
+  require("which-key").setup(opts)
+  require("utils.keymaps-helpers").which_key_register()
 end
 
 return M
