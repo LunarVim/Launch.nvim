728742bb75293dae24e4d5d07d5a95802563b02a
arpangreat <arpanthegreat41@gmail.com>
2023-10-09 06:10:51 +0530

    as you said

---
+++ lua/user/illuminate.lua
@@ -0,0 +1,35 @@
+local M = {
+  "RRethy/vim-illuminate",
+  event = "VeryLazy",
+}
+
+function M.config()
+  require("illuminate").configure {
+    filetypes_denylist = {
+      "mason",
+      "harpoon",
+      "DressingInput",
+      "NeogitCommitMessage",
+      "qf",
+      "dirvish",
+      "minifiles",
+      "fugitive",
+      "alpha",
+      "NvimTree",
+      "lazy",
+      "NeogitStatus",
+      "Trouble",
+      "netrw",
+      "lir",
+      "DiffviewFiles",
+      "Outline",
+      "Jaq",
+      "spectre_panel",
+      "toggleterm",
+      "DressingSelect",
+      "TelescopePrompt",
+    },
+  }
+end
+
+return M
