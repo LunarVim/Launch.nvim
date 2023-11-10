3874fa875e99ec82f7d751d53cf343b5f4b0b6ba
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-11-10 15:07:14 -0300

    oth: nvim notify config from lazyvim

--- lua/user/nvim-notify.lua
+++ lua/user/nvim-notify.lua
@@ -1,28 +1,27 @@
 local M = {
  'rcarriga/nvim-notify',
   event = "VeryLazy",
-}
-
-function M.config()
-  vim.notify = require("notify")
-  require('notify').setup({
-    -- Animation style (see below for details)
-    stages = "fade_in_slide_out",
-    -- Default timeout for notifications
-    timeout = 5000,
-    -- For stages that change opacity this is treated as the highlight behind the window
-    background_colour = "#000000",
-    max_width = 80,
-    max_height = 80,
-    -- Icons for the different levels
-    icons = {
-      ERROR = "",
-      WARN = "",
-      INFO = "",
-      DEBUG = "",
-      TRACE = "✎",
+  keys = {
+    {
+      "<leader>un",
+      function()
+        require("notify").dismiss({ silent = true, pending = true })
+      end,
+      desc = "Dismiss all Notifications",
     },
-  })
-end
+  },
+  opts = {
+    timeout = 3000,
+    max_height = function()
+      return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.75)
+    end,
+    on_open = function(win)
+      vim.api.nvim_win_set_config(win, { zindex = 100 })
+    end,
+  }
+}
 
 return M
