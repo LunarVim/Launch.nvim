202c910ae52bbd8c1318cdcdf9adc96b3b59490c
mdhananjay <dhananjay21iitd@gmail.com>
2024-01-26 01:31:55 +0530

    null ls deleted

--- lua/user/lspsettings/gopls.lua
+++ lua/user/lspsettings/gopls.lua
@@ -1,10 +1,26 @@
 return {
-  seftings = {
+  settings = {
     gopls = {
       analyses = {
         unusedparams = true,
       },
       staticcheck = true,
+      format = {
+        goimports = true,
+        goimports_reviser = true,
+      },
+      completion = {
+        usePlaceholders = true,
+        completeUnimported = true,
+      },
+      hoverKind = "FullDocumentation",
+      semanticTokens = true,
+      codelenses = {
+        generate = true,
+        gc_details = true,
+        tidy = true,
+        upgrade_dependency = true,
+      },
     },
   },
 }
