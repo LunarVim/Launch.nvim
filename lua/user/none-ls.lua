6ab5cd5a5bbe7a2e92d77914d1a7fb5ba844082d
mdhananjay <dhananjay21iitd@gmail.com>
2024-02-17 09:47:47 +0530

    none-ls formattors updated

--- lua/user/none-ls.lua
+++ lua/user/none-ls.lua
@@ -10,21 +10,36 @@ function M.config()
 
 	local formatting = null_ls.builtins.formatting
 	local diagnostics = null_ls.builtins.diagnostics
+	local completion = null_ls.builtins.completion
+	local actions = null_ls.builtins.code_actions
 
 	null_ls.setup {
 		debug = false,
 		sources = {
-			formatting.stylua,
-			formatting.prettier,
+			-- LUA
+			-- formatting.stylua,
+
+			-- golang
+			formatting.goimports_reviser,
+			formatting.gofumpt,
+			formatting.golines,
+
+			diagnostics.golangci_lint,
+			actions.gomodifytags,
+
+			formatting.eslint,
+
+			-- PYTHON
+			diagnostics.flake8,
 			formatting.black,
-			-- formatting.prettier.with {
-			--   extra_filetypes = { "toml" },
-			--   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-			-- },
-			-- formatting.eslint,
-			null_ls.builtins.diagnostics.flake8,
-			-- diagnostics.flake8,
-			null_ls.builtins.completion.spell,
+
+			formatting.prettier.with {
+				extra_filetypes = { "toml" },
+				extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+			},
+
+			-- GENERAL
+			completion.spell,
 		},
 	}
 end
