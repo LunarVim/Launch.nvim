fe25dc38ffcc526e7ba2d940ac4790a0b29db20b
mdhananjay <dhananjay21iitd@gmail.com>
2024-02-17 09:47:28 +0530

    colorscheme added

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,11 @@
 local M = {
 	"LunarVim/darkplus.nvim",
-	lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	dependencies = {
 		"sainnhe/gruvbox-material",
 		"cryptomilk/nightcity.nvim",
-		-- "luisiacc/gruvbox-baby",
+		"luisiacc/gruvbox-baby",
 		"sainnhe/everforest",
 		"EdenEast/nightfox.nvim",
 	},
