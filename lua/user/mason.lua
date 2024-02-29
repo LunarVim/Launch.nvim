ef6dff9654b80335b91dbc028e27230731185661
mdhananjay <dhananjay21iitd@gmail.com>
2024-02-29 20:40:17 +0530

    mason ui change

--- lua/user/mason.lua
+++ lua/user/mason.lua
@@ -20,6 +20,11 @@ function M.config()
 	require("mason").setup {
 		ui = {
 			border = "rounded",
+			icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
 		},
 	}
 	require("mason-lspconfig").setup {}
