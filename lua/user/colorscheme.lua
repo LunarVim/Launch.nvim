de765f4ef637764edd8c1ddf317dde0734097f4a
mdhananjay <dhananjay21iitd@gmail.com>
2024-04-12 17:10:28 +0530

    colorscheme gruvbox

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -8,12 +8,38 @@ local M = {
 		"luisiacc/gruvbox-baby",
 		"sainnhe/everforest",
 		"EdenEast/nightfox.nvim",
+		"folke/tokyonight.nvim",
 		{ "rose-pine/neovim", name = "rose-pine" },
-		{ "EdenEast/nightfox.nvim" } -- lazy
+		"Shatur/neovim-ayu",
+		"olimorris/onedarkpro.nvim",
 	},
 }
 
 function M.config()
+	-- Example config in Lua
+	-- vim.g.gruvbox_baby_function_style = "italic"
+	vim.g.gruvbox_baby_keyword_style = "italic"
+	-- vim.g.gruvbox_baby_background_color = "dark"
+	vim.g.gruvbox_baby_transparent_mode = "true"
+	vim.g.gruvbox_baby_use_original_palette = true
+
+
+	-- Each highlight group must follow the structure:
+	-- ColorGroup = {fg = "foreground color", bg = "background_color", style = "some_style(:h attr-list)"}
+	-- See also :h highlight-guifg
+	-- Example:
+	-- vim.g.gruvbox_baby_highlights = { Normal = { fg = "#123123", bg = "NONE", style = "underline" } }
+
+	-- Enable telescope theme
+	-- vim.g.gruvbox_baby_telescope_theme = 1
+
+	-- Load the colorscheme
+	-- require("onedarkpro").setup({
+	-- 	highlights = {
+	-- 		CmpMenuSelected = { fg = "#FF0000", bg = "#FFFF00", italic = true }
+	-- 	}
+	-- })
+
 	vim.cmd.colorscheme "gruvbox-baby"
 end
 
--- lua/user/navic.lua
+++ lua/user/navic.lua
@@ -9,6 +9,12 @@ function M.config()
 		highlight = true,
 		lsp = {
 			auto_attach = true,
+			preference = {
+				-- Assuming your template server is named "template_ls"
+				templ_ls = 10,
+				-- Assuming your HTML server is named "html_ls"
+				html_ls = 5,
+			},
 		},
 		click = true,
 		separator = " " .. icons.ui.ChevronRight .. " ",
