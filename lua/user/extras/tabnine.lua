3146111c44fba9609e0156646e4327d2f1fc417f
mdhananjay <dhananjay21iitd@gmail.com>
2024-04-12 17:10:49 +0530

    tabnine intigrate

--- lua/user/extras/tabnine.lua
+++ lua/user/extras/tabnine.lua
@@ -1,11 +1,19 @@
 local M = {
-	"tzachar/cmp-tabnine",
-	event = "InsertEnter",
-	build = "./install.sh",
+	'codota/tabnine-nvim',
+	build = "./dl_binaries.sh"
 }
 
-function M.config() end
+
+function M.config()
+	require('tabnine').setup({
+		disable_auto_comment = true,
+		accept_keymap = "<Tab>",
+		dismiss_keymap = "<C-]>",
+		debounce_ms = 800,
+		suggestion_color = { gui = "#808080", cterm = 244 },
+		exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+		log_file_path = nil, -- absolute path to Tabnine log file
+	})
+end
 
 return M
--- {
--- },
