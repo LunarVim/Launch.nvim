bf119c8d8f60d3e037f2a7131f95a434d16179c7
mdhananjay <dhananjay21iitd@gmail.com>
2024-04-12 14:07:41 +0530

    telescope fuzzy search

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -107,7 +107,7 @@ function M.config()
 		},
 		extensions = {
 			fzf = {
-				fuzzy = true,           -- false will only do exact matching
+				fuzzy = true,       -- false will only do exact matching
 				override_generic_sorter = true, -- override the generic sorter
 				override_file_sorter = true, -- override the file sorter
 				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
