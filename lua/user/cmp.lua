838ab973efd9a06e69151401d07b58ee84e4c778
mdhananjay <dhananjay21iitd@gmail.com>
2024-05-10 19:50:27 +0530

    cmp tab to c-j/k

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -62,8 +62,8 @@ function M.config()
 			end,
 		},
 		mapping = cmp.mapping.preset.insert {
-			["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-			["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+			-- ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+			-- ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
 			["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
 			["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
@@ -76,7 +76,7 @@ function M.config()
 			-- Accept currently selected item. If none selected, `select` first item.
 			-- Set `select` to `false` to only confirm explicitly selected items.
 			["<CR>"] = cmp.mapping.confirm { select = true },
-			["<Tab>"] = cmp.mapping(function(fallback)
+			["<C-j>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
 				elseif luasnip.expandable() then
@@ -94,7 +94,7 @@ function M.config()
 				"i",
 				"s",
 			}),
-			["<S-Tab>"] = cmp.mapping(function(fallback)
+			["<C-k>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_prev_item()
 				elseif luasnip.jumpable(-1) then
