8429f41504e98354cc3ea2df3f45a63de0dcb946
Tom Syroid <tsyroid@gmail.com>
2024-05-28 11:04:15 -0700

    Fix: comment out (lspconfig.lua) inlay hints to remove error

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -22,9 +22,9 @@ end
 M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
 
-  if client.supports_method "textDocument/inlayHint" then
-    vim.lsp.inlay_hint.enable(bufnr, true)
-  end
+  -- if client.supports_method "textDocument/inlayHint" then
+  --   vim.lsp.inlay_hint.enable(bufnr, true)
+  -- end
 end
 
 function M.common_capabilities()
