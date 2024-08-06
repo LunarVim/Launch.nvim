74820f1efee6af4c96557687290311706b63fd9f
Michael Weitzel <mich@elweitzel.de>
2024-08-06 21:21:54 +0200

    prefer explicit confirmation of suggestions

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -75,7 +75,7 @@ function M.config()
       },
       -- Accept currently selected item. If none selected, `select` first item.
       -- Set `select` to `false` to only confirm explicitly selected items.
-      ["<CR>"] = cmp.mapping.confirm { select = true },
+      ["<CR>"] = cmp.mapping.confirm { select = false },
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
